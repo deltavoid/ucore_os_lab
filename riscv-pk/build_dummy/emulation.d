d2c4b97103833eaac7abf14ac8ae542729575af2
zqy <850074816@qq.com>
2018-07-27 14:56:59 +0800

    add build_dummy

---
+++ riscv-pk/build_dummy/emulation.d
@@ -0,0 +1,15 @@
+emulation.o: ../machine/emulation.c ../machine/emulation.h \
+ ../machine/encoding.h ../machine/bits.h config.h \
+ ../machine/unprivileged_memory.h ../machine/mtrap.h
+
+../machine/emulation.h:
+
+../machine/encoding.h:
+
+../machine/bits.h:
+
+config.h:
+
+../machine/unprivileged_memory.h:
+
+../machine/mtrap.h:
---
+++ riscv-pk/build_dummy/misaligned_ldst.d
@@ -0,0 +1,15 @@
+misaligned_ldst.o: ../machine/misaligned_ldst.c ../machine/emulation.h \
+ ../machine/encoding.h ../machine/bits.h ../machine/unprivileged_memory.h \
+ ../machine/mtrap.h config.h
+
+../machine/emulation.h:
+
+../machine/encoding.h:
+
+../machine/bits.h:
+
+../machine/unprivileged_memory.h:
+
+../machine/mtrap.h:
+
+config.h:
